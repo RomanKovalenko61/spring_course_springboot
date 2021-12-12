7987671d70904ab6a2419073a04ed0169a5b1aa1
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 17:20:55 +0300

    Course: Spring for beginners

---
+++ src/main/java/com/zaurtregulov/springboot/spring_course_springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.springboot.spring_course_springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringCourseSpringbootApplication.class);
+    }
+
+}
